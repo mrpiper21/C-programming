4108ce07bda61d03d0dabb96e1bdebe7ce5d0c94
mrpiper21 <benbaah104@gmail.com>
2023-04-02 16:57:10 +0000

    pointers

---
+++ pointers/p1.c
@@ -0,0 +1,13 @@
+/**
+ * Pointers and arrays
+ */
+#include <stdio.h>
+int main(void)
+{
+	int c[] = {3, 6, 8, 2, 9};
+	printf("%d\n", c); /*address of the first element in the array*/
+	printf("%d\n", &c[0]); /*Also prints the address of the first element in the array*/
+	printf("%d\n", c[0]); /*prints the first element in the array*/
+	printf("%d\n", *c); /*print the first element in the array*/
+	return 0;
+}
