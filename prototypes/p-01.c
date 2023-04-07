9ec7f28c25c6d6cf4d9bbc6266f02a3bae54a1da
mrpiper21 <benbaah104@gmail.com>
2023-04-07 18:28:19 +0000

    Sorted

--- prototypes/p-01.c
+++ prototypes/p-01.c
@@ -1,51 +1,55 @@
 #include <stdio.h>
 
-int sort(int a, int b, int c);
+/*sort - sort given values in an assending order*/
+
+void sort(int a, int b, int c)
+{
+        int num1, num2, num3;
+
+        if (a < b && a < c){
+		num1 = a;
+        	if (b < c){
+                	num2 = b;
+                	num3 = c;
+			printf("The sorted values are: %d, %d, %d\n", num1, num2, num3);
+		}else{
+			num2 = c;
+			num3 = b;
+			printf("The sorted values are: %d, %d, %d\n", num1, num2, num3);
+		}
+    	}
+	if (b < a && b < c){
+		num1 = b;
+        	if (a < c){
+            		num2 = a;
+            		num3 = c;
+			printf("The sorted values are: %d, %d, %d\n", num1, num2, num3);
+        	}
+		else{
+			num2 = c;
+			num3 = a;
+			printf("The sorted values are: %d, %d, %d\n", num1, num2, num3);
+		}
+    	}
+	if (c < a && c < b){
+		num1 = c;
+        	if (b < a){
+            		num2 = b;
+            		num3 = a;
+			printf("The sorted values are: %d, %d, %d\n", num1, num2, num3);
+        	}
+		else{
+			num2 = a;
+			num3 = b;
+			printf("The sorted values are: %d, %d, %d\n", num1, num2, num2);
+		}
+	}
+}
 
 int main(void)
 {
-	int j, q, b;
-	printf("first num: \n");
-	scanf("%d", &j);
-	printf("second num: \n");
-	scanf("%d", &q);
-	printf("Third num: \n");
-	scanf("%d", &b);
+	int m = 4, v = -5, p = 2;
+	sort(m, v, p);
 
-	printf("sorted = %d", sort(j, q, b));
-}
-
-int sort(int a,int b, int c)
-{
-	int num1, num2, num3;
-
-	if (a < b && a < c)
-	{
-		num1 = a;
-		if (b < c)
-		{
-			num2 = b;
-			num3 = c;
-		}
-	}
-	if (b < a && b < c)
-	{
-		num1 = b;
-		if (a < c)
-		{
-			num2 = a;
-			num3= c;
-		}
-	}
-	if (c < a && c < b)
-	{
-		num1 = c;
-		if (b < a)
-		{
-			num2 = b;
-			num3 = a;
-		}
-	}
-
-	return (num1, num2, num3);
+	;return 0;
 }
