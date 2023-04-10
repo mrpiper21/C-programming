fbcf449cf7a9a115177367afc6f23e4e0afca9f9
mrpiper21 <benbaah104@gmail.com>
2023-04-10 03:51:08 +0000

    more pointers

---
+++ pointers/p-f.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+//Pointers to Functions
+//A pointer can be declared to point to a function. The declaration notation is a bit cryptic.The following illustrates how to declare a pointer to a function. The function is passed void and returns void. The pointer’s name is foo:
+
+void (*foo)();
