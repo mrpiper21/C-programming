3e9ae7f9095fb2c5421478c687df12116919adb0
mrpiper21 <benbaah104@gmail.com>
2023-04-30 20:27:57 +0000

    Arrays

--- Introduction/Arrays/ax_02.c
+++ Introduction/Arrays/ax_02.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-    int score1 = 79;
-    int score2 = 48;
-    int score3 = 99;
+    int scores[3];
+    scores[0] = 57;
+    scores[1] = 99;
+    scores[2] = 84;
 
-    printf("Average score: %f\n", (score1 + score2 + score3)/ 3.0);
+    printf("Average score: %f\n", (scores[0] + scores[1] + scores[2])/ 3.0);
+
+    //OR
+
+    int score[3];
+
+    printf("SCORE: ");
+    for (int i = 0; i < 3; i++){
+        scanf("%d", &score[i]);
+    }printf("Average score: %f\n", (score[0] + score[1] + score[2])/ 3.0);
 }
