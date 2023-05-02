5cc62ead897e99b8053031a9eee51a24e663a946
mrpiper21 <benbaah104@gmail.com>
2023-05-02 09:49:47 +0000

    Arrays

--- variable_scope/local_variables.c
+++ variable_scope/local_variables.c
@@ -3,9 +3,9 @@
 int tripple(int x); // declaration
 
 int main(void){
-    int result = tripple(5); // var result id local to main
+    int result = tripple(5); // var result id local to main()
 }
 
 int tripple(int x){
-    return x * 3; // var x is local to fx tripple
+    return x * 3; // var x is local to fx tripple()
 }
