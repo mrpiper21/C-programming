404fee03792ba234f7ba7b915181f313fca8dc71
piper <benbaah104@gmail.com>
2023-06-15 14:01:03 +0000

    Caesser algorithm update

--- Introduction/Arrays/Caesar.c
+++ Introduction/Arrays/Caesar.c
@@ -9,40 +9,38 @@ char upper[] = {"ABCDEFGHIjkLMNOPQRSTUVWXYZ"};
 
 
 
-int num[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
+int num[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 int pi, len;
 
 int main(int argc, char *argv[]){
     int key, cyphavalue;
     char cyphar_text[50];
-    char plain_text[50];
-    for (int i = 0; i < 10; i++){
-        if (num[i] == argv[1]){
-            key = i;
-            printf("Plain text: ");
-            gets(plain_text);
-            len = strlen(plain_text);
-            printf("ciphertext: ");
-            for (pi = 0; pi < len; pi++){
-                for (int j = 0; j < 26; j++){
-                    if (plain_text[pi] == alphabets[j]){
-                        cyphavalue = (key + j) % 26;
-                        cyphar_text[pi]= alphabets[cyphavalue];
-                    } if (plain_text[pi] == upper[j]){
-                        cyphavalue = (key + j) % 26;
-                        cyphar_text[pi]= upper[cyphavalue]; 
-                    } else {
-                        cyphar_text[pi] = plain_text[j];
-                    }
+
+    key = (int)atoi(argv[argc - 1]);
+    int v = (int)key;
+    if (v >= 48 && v <= 57) {
+        char plain_text[50];
+        printf("Plain text: ");
+        gets(plain_text);
+        len = strlen(plain_text);
+        printf("ciphertext: ");
+        for (pi = 0; pi < len; pi++){
+            for (int j = 0; j < 26; j++){
+                if (plain_text[pi] == alphabets[j]){
+                    cyphavalue = (key + j) % 26;
+                    cyphar_text[pi]= alphabets[cyphavalue];
+                }else if (plain_text[pi] == upper[j]){
+                    cyphavalue = (key + j) % 26;
+                    cyphar_text[pi]= upper[cyphavalue]; 
                 }
-                printf("%c", cyphar_text[pi]);
-            }
-        } else {
-            printf("%d\n", *argv[1]);
-            printf("Value must be a digit!\n");
-            break;
+            }printf("%c", cyphar_text[pi]);
         }
+    } else {
+        printf("Value must be a digit");
     }
+
+
+    
     return 0;
 }
 
