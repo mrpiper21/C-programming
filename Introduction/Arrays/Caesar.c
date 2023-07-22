34d442654a2a91e24721cf8eb45367d1b3d14e60
piper <benbaah104@gmail.com>
2023-07-22 16:55:12 +0000

    Final update on Caeser's algorithm

--- Introduction/Arrays/Caesar.c
+++ Introduction/Arrays/Caesar.c
@@ -1,49 +1,42 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
 
-//int value[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
-char alphabets[] = {"abcdefghijklmnopqrstuvwxyz"};
-char upper[] = {"ABCDEFGHIjkLMNOPQRSTUVWXYZ"};
+char alphabets[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char upper[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-
-
-int num[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-int pi, len;
-
-int main(int argc, char *argv[]){
-    int key, cyphavalue;
+int main(int argc, char* argv[]) {
+    int key;
     char cyphar_text[50];
 
-    key = (int)atoi(argv[argc - 1]);
-    int v = (int)key;
-    printf("%d", v);
-    if (v >= 1 && v <= 26) {
+    key = atoi(argv[argc - 1]);
+
+    if (key >= 1 && key <= 26) {
         char plain_text[50];
+
         printf("Plain text: ");
-        gets(plain_text);
-        len = strlen(plain_text);
-        printf("ciphertext: ");
-        for (pi = 0; pi < len; pi++){
-            }for (int i = 0; i < 26; i++){
-                if (plain_text[pi] == upper[i]){
-                    cyphavalue = (key + i) % 26;
-                    cyphar_text[pi] = upper[cyphavalue];
-                }
-            for (int j = 0; j < 26; j++){
-                if (plain_text[pi] == alphabets[j]){
-                    cyphavalue = (key + j) % 26;
-                    cyphar_text[pi] = alphabets[cyphavalue];
-                }
-            }printf("%c", cyphar_text[pi]);
+        fgets(plain_text, sizeof(plain_text), stdin);
+        plain_text[strcspn(plain_text, "n")] = '0';
+
+        size_t len = strlen(plain_text) + 1;
+
+        printf("Ciphertext: ");
+        for (size_t pi = 0; pi < len; pi++) {
+            if (plain_text[pi] >= 'A' && plain_text[pi] <= 'Z') {
+                int cyphavalue = (key + (plain_text[pi] - 'A')) % 26;
+                cyphar_text[pi] = upper[cyphavalue];
+            } else if (plain_text[pi] >= 'a' && plain_text[pi] <= 'z') {
+                int cyphavalue = (key + (plain_text[pi] - 'a')) % 26;
+                cyphar_text[pi] = alphabets[cyphavalue];
+            } else {
+                cyphar_text[pi] = plain_text[pi];
+            }
         }
-    } else {
-        printf("Value must be a digit");
+
+        printf("%sn", cyphar_text);
     }
 
-
-    
     return 0;
 }
-
